2cd4634d5dd502a8add2ea1e108913bbf4f1801b
Peter Ade-Ojo <adeojopeter@gmail.com>
2023-03-28 15:04:01 +0100

    user interface and model

---
+++ Backend/src/models/User/.d.ts
@@ -0,0 +1,10 @@
+import { Document } from 'mongoose';
+
+export default interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+  avatar?: string;
+  date: Date;
+  provider: "google" | "facebook" | "local";
+}
--- Backend/src/models/User/index.ts
+++ Backend/src/models/User/index.ts
@@ -1,1 +1,35 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+
+import IUser from './.d';
+
+const userSchema = new Schema<IUser>({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  provider: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+const User = mongoose.model<IUser>('User', userSchema);
+export default User;
